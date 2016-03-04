9eceda869de47fc6e246fa9589b9fc1ad9b6cb45
JJ Asghar <jj@chef.io>
2016-03-04 16:36:22 -0600

    added better metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,10 @@ maintainer "JJ Asghar"
 maintainer_email "jj@chef.io"
 license "Apache 2.0"
 description "Sets everything you need up for reveal-md"
-version "0.1.0"
-issues_url "https://github.com/jjasghar/reveal-md-cookbook/issues"
-source_url "https://github.com/jjasghar/reveal-md-cookbook/"
+long_description "Sets everything you need up for reveal-md"
+version "0.1.1"
+
+issues_url 'https://github.com/jjasghar/reveal-md-cookbook/issues'
+source_url 'https://github.com/jjasghar/reveal-md-cookbook/'
+
+supports 'ubuntu' , ">= 14.04"
